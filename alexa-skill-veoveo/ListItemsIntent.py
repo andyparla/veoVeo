5031633ebd311283c4af9f688504350936776ea1
andyparla <andyparla@gmail.com>
2021-11-20 23:34:02 +0100

    modificaciones para crear un diccionario de objetos casero

--- alexa-skill-veoveo/ListItemsIntent.py
+++ alexa-skill-veoveo/ListItemsIntent.py
@@ -1,7 +1,8 @@
 from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
 from ask_sdk_core.utils import is_intent_name
 import logging
-from random import sample
+from random import sample, random
+from diccionario import objetos
 import six
 
 """
@@ -10,29 +11,24 @@ clase que define realmente lo que hace nuestra skill
 
 
 class ListItemsIntent(AbstractRequestHandler):
-    def __init__(self):
-        self.searchObjects = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-                         "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s",
-                         "t", "u", "v", "w", "x", "y", "z"]
-
     def can_handle(self, handler_input):
         return is_intent_name("ListItemsIntent")(handler_input)
 
     def handle(self, handler_input):
         slots = handler_input.request_envelope.request.intent.slots
-        defaultObjsToSearch = 3
 
         for slotName, currentSlot in six.iteritems(slots):
             if slotName == 'letra':
                 if currentSlot.value:
-                    objs_to_search = sample(self.searchObjects, int(currentSlot.value))
+                    objs_could_see = random.choice(objetos.get(currentSlot.value))
                 else:
-                    objs_to_search = sample(self.searchObjects, defaultObjsToSearch)
+                    #objs_could_see = sample(self.searchObjects, defaultObjsToSearch)
+                    logging.error("No se ha detectado la letra indicada")
         speech_text = "<say-as interpret-as=\"interjection\">Magnífico!</say-as>. Aquí van, prestad atención: {0}. A " \
                       "divertirse!. <say-as interpret-as=\"interjection\">Suerte!</say-as>." \
-            .format(", ".join(objs_to_search))
+            .format(", ".join(objs_could_see))
 
-        logging.info(objs_to_search)
+        logging.info(objs_could_see)
         logging.info(speech_text)
 
         return handler_input.response_builder.speak(speech_text).response
